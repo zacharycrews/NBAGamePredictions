e0338269d13268087d932773308ab9c25a7ce325
Zach Crews <crewsz@bc.edu>
2022-12-04 16:41:43 -0500

    Add features

--- predictions.py
+++ predictions.py
@@ -5,9 +5,11 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
 from sklearn.linear_model import LogisticRegression
 
+month_lengths = {10:31,11:30,12:31,1:31,2:28,3:31,4:30,5:31,6:31}
 
+### TRAIN MODEL ###
 
-game_features = [[0,0,0,0,0,0,0,0,0] for i in range(len(training_games))]
+game_features = [[0,0,0,0,0,0,0,0] for i in range(len(training_games))]
 
 for i, game in enumerate(training_games):
     home_team = game['home']
@@ -37,13 +39,44 @@ for i, game in enumerate(training_games):
             away_pts += int(stats['visitor_PTS'])
             away_pa += int(stats['home_PTS'])
             away_games += 1
+
     game_features[i][1] = home_pts / max(home_games,1)
     game_features[i][2] = home_pa / max(home_games,1)
     game_features[i][5] = away_pts / max(away_games,1)
     game_features[i][6] = away_pa / max(away_games,1)
 
+    # Determine if it is team's second game in a row
+    start_date = game['start_date_time'].split("-")
+    current_date = start_date.copy()
+    current_date = "-".join(current_date[:3])
+    if int(start_date[2]) > 0:
+        start_date[2] = str(int(start_date[2]) - 1)
+        previous_date = "-".join(start_date[:3])
+    else:
+        if int(start_date[1]) == 1:         # New Years
+            start_date[0] = str(int(start_date[0]) - 1)
+            start_date[1] = "12"
+            start_date[2] = "30"
+            previous_date = "-".join(start_date[:3])
+        else:                               # First day of month
+            start_date[1] = str(int(start_date[1]) - 1)
+            start_date[2] = str(month_lengths[int(start_date[1])] - 1)
+            previous_date = "-".join(start_date[:3])
+    for j in range(i-1, -1, -1):
+        prev_game = training_games[j]
+        if previous_date in prev_game['start_date_time'] or current_date in prev_game['start_date_time']:
+            if (prev_game['home'] == home_team or prev_game['visitor'] == home_team) and previous_date in prev_game['start_date_time']:
+                game_features[i][3] = 1
+            if (prev_game['home'] == away_team or prev_game['visitor'] == away_team) and previous_date in prev_game['start_date_time']:
+                game_features[i][7] = 1
+        else:
+            break
+        
     # Get sentiment score
-    
+
+
+for i in range(100):
+    print(str(i) + ":\t" + str(game_features[i]))
 X_train = game_features
 Y_train = training_labels
 
