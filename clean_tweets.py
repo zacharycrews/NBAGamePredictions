e63a0e2b9cbb37ed2dda942949be995e4016767e
Danh Ho <hodj@bc.edu>
2022-12-04 21:02:37 -0500

    Final Preprocessing Version

--- clean_tweets.py
+++ clean_tweets.py
@@ -1,37 +1,44 @@
 import re
 import nltk
-
-
-val = input("Enter 'home' or 'away': ")
-for i in range(1323):
-    path = 'dataset/'+val+'tweets'+val+i+'.txt'
-    with open(path, 'r', encoding="utf-8") as file:
-        tweets = file.read()
-    with open(path, 'w+', encoding="utf-8") as file:
-        file.write(clean_tweets(tweets))
-
+import json
 
 def clean_tweets(tweets):
 
     ### Cleaning Data
-    tweets = re.sub("\n", " ", tweets)
     # Remove mentions
-    tweets = re.sub(r'[A-Za-z0-9]*@[A-Za-z]*\.?[A-Za-z]*',' ',tweets)
+    tweets = re.sub(r'[A-Za-z0-9]*@[A-Za-z_]*\.?[A-Za-z]*',' ',tweets)
+    # Remove Emojis
+    tweets = re.sub(r'\\\w+', ' ', tweets)
     # Remove url 
     tweets = re.sub(r'http\S+', ' ' ,tweets)
     # Remove hashtags
-    tweets = re.sub(r'[@#][A-Za-z0-9_]{3,}',' ',tweets)
+    tweets = re.sub(r'[@#][A-Za-z0-9_]+',' ',tweets)
     # Remove all non-word characters
     tweets = re.sub('[^A-Za-z]',' ',tweets)
     # Remove multiple space with single space
-    tweets = re.sub(' +', ' ', tweets)
+    # tweets = re.sub(' +', ' ', tweets)
     
-    ### Tokenizing and Lemmatizing
+    ### Tokenizing
     # Tokenize
     tokens = nltk.word_tokenize(tweets)
-    # Lemmatize
-    lemmatizer = nltk.stem.WordNetLemmatizer()
-    for i in range(len(tokens)):
-        tokens[i] = lemmatizer.lemmatize(tokens[i])
+    
+    # Remove Handcrafted Stopwords
+    stop_words = {"d", "ll", "m", "ve", "t", "s", "re"}
+    filtered = [w for w in tokens if not w.lower() in stop_words]
 
-    return tokens
+    # List into String
+    final_tweets = ' '.join(filtered)
+    return final_tweets
+
+home_away = input("Enter 'home' or 'away': ")
+train_test = input("Enter 'train' or 'test': ")
+
+for i in range(247):
+    path = 'dataset/'+home_away+'_'+train_test+'/'+home_away+'_'+train_test+str(i)+'.json'
+    with open(path, 'r') as in_file:
+        tweets = json.load(in_file)
+    with open(path, 'w') as out_file:
+        for k in tweets.keys():
+            cleaned = clean_tweets(tweets[k])
+            tweets[k] = cleaned
+        json.dump(tweets, out_file)
